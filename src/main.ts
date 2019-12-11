da7c75b3a55fbaaaa137965188dd403622dc54ad
Carolina Nobre <carolinanobre@gmail.com>
2019-12-11 09:41:40 -0700

    updated readme

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { Greeter } from './greeter';
 import './styles/styles.css';
-import { initProvenance } from '@visdesignlab/provenance-lib-core/lib';
+import { initProvenance } from '@visdesignlab/provenance-lib-core';
 
 const g = new Greeter('Juri');
 g.greet();
 
-initProvenance();
-document.querySelector('body').innerHTML = 'Nobre';
+// initProvenance();
+document.querySelector('body').innerHTML = 'cnobre';
